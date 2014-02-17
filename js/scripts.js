fec178c5b7f0182be92fd3e1a3c5e12627ea0356
Michael Nugent <nugent.michael@gmail.com>
2014-02-17 17:36:39 -0500

    Fixed Layout and Scrolling Problem
    Fixed that pesky scrolling problem and some adjustments to the layout
    on desktop devices.

--- js/scripts.js
+++ js/scripts.js
@@ -29,7 +29,7 @@ $(function() {
         $(this).siblings("div").fadeOut(200);
       }
       details_closed = !details_closed;
-    })
+    });
 
   $('.flexslider').flexslider({
     animation: "slide",

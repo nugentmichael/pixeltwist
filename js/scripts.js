56bdbc408ca507a9d3fef957e8ebbcf6f35096a2
nugentmichael <nugent.michael@gmail.com>
2019-06-26 11:29:26 -0400

    Revise portfolio JSON array structure.

--- js/scripts.js
+++ js/scripts.js
@@ -1,78 +1,78 @@
 // JavaScript Document
 
 // JSON Data
-[{
-        "portfolio": {
-            "examples": {
-                "backflipStudios": [{
-                    "description": "Backflip Studios is a production company headquartered in Ottawa with popular recent Canada-wide releases. They wanted to promote their most ambitious project yet, which is where we came in."
-                }],
-                "theBluePepperGrill": [{
-                    "description": "With locations across Ontario and looking beyond, The Blue Pepper Grill needed a fresh face to lead it’s growth to western Canada."
-                }],
-                "peacefulPlay": [{
-                    "description": "In order to spread their work on a global scale Peaceful Play had to work hard to break into the crowded not-for-profit sector. We were more than happy to accompany them in Africa to help shoot video for use on their website."
-                }],
-                "mercuryInsurance": [{
-                    "description": "Being the most popular insurance company among young drivers, Mercury Insurance decided to target its audience in a campaign to reduce distracted driving. We knew we just had to help out with this very worthwhile cause."
-                }],
-                "composure": [{
-                    "description": "In one of our most ambitious projects yet, we worked with the creators of the upcoming release, Composure, helping them produce and edit teaser trailers. Along with this, we developed their web presence as well as their marketing plan for the release of Composure."
-                }]
-            }
-        }
-    },
-
-
+portfolio = [
     {
-        "staff": {
-            "profile": {
-                "aisha": [{
-                    "clear": "images/team/clear/aisha.jpg",
-                    "pixelated": "images/team/pixelated/aisha.jpg",
-                    "bio": "is a crafty designer who perpetually strives for pixel perfection. She believes that the first idea is never the best. Often she isn’t content until she has made upwards of 40 revisions. With the help of some Pixeltwist coworkers, she is peeled away from the computer screen long enough to enjoy a green tea frappuccino. That moment is fleeting though, as she quickly finds her way back to her computer to start on revision 41."
-                }],
-                "keenan": [{
-                    "clear": "images/team/clear/keenan.jpg",
-                    "pixelated": "images/team/pixelated/keenan.jpg",
-                    "bio": "enjoys life through the view of a camera lens. Everywhere he goes he sees shot angles, backdrops and set pieces. It’s this half-man, half-camera cyborg that leads the video department at Pixeltwist. When he isn’t in the studio producing amazing video for clients, Keenan enjoys expanding his already expanded movie knowledge."
-                }],
-                "kevin": [{
-                    "clear": "images/team/clear/kevin.jpg",
-                    "pixelated": "images/team/pixelated/kevin.jpg",
-                    "bio": "refers to himself as the web wizard at Pixeltwist. While it may seem a touch immodest to refer to himself as a wizard, his talent for flawless code cannot be denied. Some say his ability comes from being a direct descendant of an ancient coding god. The reality is that nobody has seen Kevin leave the studio in a long while. It is believed that he naps in the ceiling at night and emerges in the wee hours of the morning to feed his coding compulsions."
-                }],
-                "lulu": [{
-                    "clear": "images/team/clear/lulu.jpg",
-                    "pixelated": "images/team/pixelated/lulu.jpg",
-                    "bio": "is project-lead at Pixeltwist. She’s plugged in to everything going on through each day. When a client comes looking for our expertise, she’s spearheading the meeting to extract the perfect idea. Her attention to detail extends to the kitchen, where she often brings some of her masterfully-prepared home-cooked food to the office for everyone to enjoy."
-                }],
-                "michael": [{
-                    "clear": "images/team/clear/michael.jpg",
-                    "pixelated": "images/team/pixelated/michael.jpg",
-                    "bio": "is the User Experience Designer in charge of keeping the peace between design and usability. His passion is in walking the fine line between a beautiful design and a smart design. His word is the law with keeping UX beautiful, but above all else, functional. So if any designer hopes to get their work approved, they have to get past Michael first."
-                }],
-                "robb": [{
-                    "clear": "images/team/clear/robb.jpg",
-                    "pixelated": "images/team/pixelated/robb.jpg",
-                    "bio": "is the lead audio guy at Pixeltwist. Much like a dog can pick up sounds that humans can’t, he can pick up minute imperfections in sound that the rest of us mere mortals cannot. During his job interview we had to take his word for it and hire him. Robb’s passion was forged through years of being a musician and music lover."
-                }],
-                "vanessa": [{
-                    "clear": "images/team/clear/vanessa.jpg",
-                    "pixelated": "images/team/pixelated/vanessa.jpg",
-                    "bio": "is the savvy web designer at Pixeltwist. She’s the web developer embodiment of a swiss army knife. She can knock out fresh new ideas in photoshop, mock them up to work on the web, then present them to the client all in a days work. But aside from school and years of professional experience, where does she get such ability? The word is her sketchbook was imbued by ancient gypsies granting her mystical web development skill."
-                }],
-                "zach": [{
-                    "clear": "images/team/clear/vanessa.jpg",
-                    "pixelated": "images/team/pixelated/vanessa.jpg",
-                    "bio": "is the content manager at Pixeltwist. Wrangling words like a skillful literary cowboy is how he spends his day. But to put it in plain english, he is the lead copywriter and walking, talking dictionary at the studio. The creative part of Zach’s brain grows three sizes when he’s twisting words for clients at Pixeltwist."
-                }]
-            }
-        }
+        "id": "backflipStudios",
+        "name": "Backflip Studios",
+        "description": "Backflip Studios is a production company headquartered in Ottawa with popular recent Canada-wide releases. They wanted to promote their most ambitious project yet, which is where we came in."
+    },
+    {
+        "id": "theBluePepperGrill",
+        "name": "The Blue Pepper Grill",
+        "description": "With locations across Ontario and looking beyond, The Blue Pepper Grill needed a fresh face to lead it’s growth to western Canada."
+    },
+    {
+        "id": "peacefulPlay",
+        "name": "Peaceful Play",
+        "description": "In order to spread their work on a global scale Peaceful Play had to work hard to break into the crowded not-for-profit sector. We were more than happy to accompany them in Africa to help shoot video for use on their website."
+    },
+    {
+        "id": "mercuryInsurance",
+        "name": "Mercury Insurance",
+        "description": "Being the most popular insurance company among young drivers, Mercury Insurance decided to target its audience in a campaign to reduce distracted driving. We knew we just had to help out with this very worthwhile cause."
+    },
+    {
+        "id": "composure",
+        "name": "Composure",
+        "description": "In one of our most ambitious projects yet, we worked with the creators of the upcoming release, Composure, helping them produce and edit teaser trailers. Along with this, we developed their web presence as well as their marketing plan for the release of Composure."
     }
-]
+];
 
-$(function() {	
+var staff = [{
+    "aisha": [{
+        "clear": "images/team/clear/aisha.jpg",
+        "pixelated": "images/team/pixelated/aisha.jpg",
+        "bio": "is a crafty designer who perpetually strives for pixel perfection. She believes that the first idea is never the best. Often she isn’t content until she has made upwards of 40 revisions. With the help of some Pixeltwist coworkers, she is peeled away from the computer screen long enough to enjoy a green tea frappuccino. That moment is fleeting though, as she quickly finds her way back to her computer to start on revision 41."
+    }],
+    "keenan": [{
+        "clear": "images/team/clear/keenan.jpg",
+        "pixelated": "images/team/pixelated/keenan.jpg",
+        "bio": "enjoys life through the view of a camera lens. Everywhere he goes he sees shot angles, backdrops and set pieces. It’s this half-man, half-camera cyborg that leads the video department at Pixeltwist. When he isn’t in the studio producing amazing video for clients, Keenan enjoys expanding his already expanded movie knowledge."
+    }],
+    "kevin": [{
+        "clear": "images/team/clear/kevin.jpg",
+        "pixelated": "images/team/pixelated/kevin.jpg",
+        "bio": "refers to himself as the web wizard at Pixeltwist. While it may seem a touch immodest to refer to himself as a wizard, his talent for flawless code cannot be denied. Some say his ability comes from being a direct descendant of an ancient coding god. The reality is that nobody has seen Kevin leave the studio in a long while. It is believed that he naps in the ceiling at night and emerges in the wee hours of the morning to feed his coding compulsions."
+    }],
+    "lulu": [{
+        "clear": "images/team/clear/lulu.jpg",
+        "pixelated": "images/team/pixelated/lulu.jpg",
+        "bio": "is project-lead at Pixeltwist. She’s plugged in to everything going on through each day. When a client comes looking for our expertise, she’s spearheading the meeting to extract the perfect idea. Her attention to detail extends to the kitchen, where she often brings some of her masterfully-prepared home-cooked food to the office for everyone to enjoy."
+    }],
+    "michael": [{
+        "clear": "images/team/clear/michael.jpg",
+        "pixelated": "images/team/pixelated/michael.jpg",
+        "bio": "is the User Experience Designer in charge of keeping the peace between design and usability. His passion is in walking the fine line between a beautiful design and a smart design. His word is the law with keeping UX beautiful, but above all else, functional. So if any designer hopes to get their work approved, they have to get past Michael first."
+    }],
+    "robb": [{
+        "clear": "images/team/clear/robb.jpg",
+        "pixelated": "images/team/pixelated/robb.jpg",
+        "bio": "is the lead audio guy at Pixeltwist. Much like a dog can pick up sounds that humans can’t, he can pick up minute imperfections in sound that the rest of us mere mortals cannot. During his job interview we had to take his word for it and hire him. Robb’s passion was forged through years of being a musician and music lover."
+    }],
+    "vanessa": [{
+        "clear": "images/team/clear/vanessa.jpg",
+        "pixelated": "images/team/pixelated/vanessa.jpg",
+        "bio": "is the savvy web designer at Pixeltwist. She’s the web developer embodiment of a swiss army knife. She can knock out fresh new ideas in photoshop, mock them up to work on the web, then present them to the client all in a days work. But aside from school and years of professional experience, where does she get such ability? The word is her sketchbook was imbued by ancient gypsies granting her mystical web development skill."
+    }],
+    "zach": [{
+        "clear": "images/team/clear/vanessa.jpg",
+        "pixelated": "images/team/pixelated/vanessa.jpg",
+        "bio": "is the content manager at Pixeltwist. Wrangling words like a skillful literary cowboy is how he spends his day. But to put it in plain english, he is the lead copywriter and walking, talking dictionary at the studio. The creative part of Zach’s brain grows three sizes when he’s twisting words for clients at Pixeltwist."
+    }]
+}];
+
+$(function() {
     var pull = $('#pull');
     menu = $('#navigation ul');
     menuHeight = menu.height();
@@ -327,7 +327,7 @@ $(function() {
                 break;
         }
 
-		var overlay = $('<div id="overlay"><div class="profile"><img src="images/team/clear/' + $(this).attr('id') + '.jpg" alt="' + $(this).attr('value') + '"><div><h3>' + $(this).attr('value') + '</h3><p>' + profile + '</p></div></div></div>');
+        var overlay = $('<div id="overlay"><div class="profile"><img src="images/team/clear/' + $(this).attr('id') + '.jpg" alt="' + $(this).attr('value') + '"><div><h3>' + $(this).attr('value') + '</h3><p>' + profile + '</p></div></div></div>');
 
         overlay.appendTo(document.body).show('slow');
 

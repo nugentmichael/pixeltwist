2c81caddb739ba10f0c492582030f48cc19b8e02
nugentmichael <nugent.michael@gmail.com>
2019-06-26 12:55:43 -0400

    Revise Team Members overlay functionality.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 // JavaScript Document
+var overlay;
 
 // JSON Data
 portfolio = [{
@@ -129,8 +130,6 @@ $(function() {
         animationLoop: false,
     });
 
-    // #backflipStudios, #theBluePepperGrill, #peacefulPlay, #mercuryInsurance, #composure
-
     $('.bubble').on('click', function() {
         switch (this.id) {
             case 'webBubble':
@@ -151,8 +150,6 @@ $(function() {
     });
 
     $('.workExamples').on('click', function() {
-        var overlay;
-
         for (var i = 0; i < portfolio.length; i++) {
 			if ($(this).attr('id') == portfolio[i].id) {
                 overlay = $('<div id="overlay">\
@@ -165,9 +162,9 @@ $(function() {
 						</div>\
 					</div>');
             }
-        }
-
-        overlay.appendTo(document.body).show('slow');
+		}
+		
+		$('body').append(overlay);
 
         if ($(this).length != 0) {
             $(overlay).on('click', function() {
@@ -208,132 +205,22 @@ $(function() {
 			</div>');
     }
 
-    $('#photos img').on('mouseenter', function() {
-        $(this).css({
-            'cursor': 'pointer',
-            'opacity': '0.5',
-            'transition': '0.3s ease-in-out',
-            '-webkit-transition': '0.3s ease-in-out'
-        }).fadeIn('slow', function() {
-            switch (this.id) {
-                case 'aisha':
-                    $(this).attr('src', 'images/team/pixelated/aisha.jpg');
-                    $('#biberdorf').css('opacity', '1.0');
-                    break;
-                case 'keenan':
-                    $(this).attr('src', 'images/team/pixelated/keenan.jpg');
-                    $('#boutilier').css('opacity', '1.0');
-                    break;
-                case 'kevin':
-                    $(this).attr('src', 'images/team/pixelated/kevin.jpg');
-                    $('#andrews').css('opacity', '1.0');
-                    break;
-                case 'lulu':
-                    $(this).attr('src', 'images/team/pixelated/lulu.jpg');
-                    $('#davies').css('opacity', '1.0');
-                    break;
-                case 'michael':
-                    $(this).attr('src', 'images/team/pixelated/michael.jpg');
-                    $('#nugent').css('opacity', '1.0');
-                    break;
-                case 'robb':
-                    $(this).attr('src', 'images/team/pixelated/robb.jpg');
-                    $('#over').css('opacity', '1.0');
-                    break;
-                case 'vanessa':
-                    $(this).attr('src', 'images/team/pixelated/vanessa.jpg');
-                    $('#burguete').css('opacity', '1.0');
-                    break;
-                case 'zach':
-                    $(this).attr('src', 'images/team/pixelated/zach.jpg');
-                    $('#falsetto').css('opacity', '1.0');
-                    break;
-                default:
-                    break;
-            }
-        });
-    });
-
-    $('#photos img').on('mouseout', function() {
-        $(this).css({
-            'cursor': 'pointer',
-            'opacity': '1.0',
-            'transition': '0.3s ease-in-out',
-            '-webkit-transition': '0.3s ease-in-out'
-        }).fadeIn('slow', function() {
-            switch (this.id) {
-                case 'aisha':
-                    $(this).attr('src', 'images/team/clear/aisha.jpg');
-                    $('#biberdorf').css('opacity', '0');
-                    break;
-                case 'keenan':
-                    $(this).attr('src', 'images/team/clear/keenan.jpg');
-                    $('#boutilier').css('opacity', '0');
-                    break;
-                case 'kevin':
-                    $(this).attr('src', 'images/team/clear/kevin.jpg');
-                    $('#andrews').css('opacity', '0');
-                    break;
-                case 'lulu':
-                    $(this).attr('src', 'images/team/clear/lulu.jpg');
-                    $('#davies').css('opacity', '0');
-                    break;
-                case 'michael':
-                    $(this).attr('src', 'images/team/clear/michael.jpg');
-                    $('#nugent').css('opacity', '0');
-                    break;
-                case 'robb':
-                    $(this).attr('src', 'images/team/clear/robb.jpg');
-                    $('#over').css('opacity', '0');
-                    break;
-                case 'vanessa':
-                    $(this).attr('src', 'images/team/clear/vanessa.jpg');
-                    $('#burguete').css('opacity', '0');
-                    break;
-                case 'zach':
-                    $(this).attr('src', 'images/team/clear/zach.jpg');
-                    $('#falsetto').css('opacity', '0');
-                    break;
-                default:
-                    break;
-            }
-        });
-    });
-
     $('#photos img').on('click', function() {
-        var profile = $(this).text;
+		for (var i = 0; i < staff.length; i++) {
+		    if ($(this).attr('id') == staff[i].id) {
+		        overlay = $('<div id="overlay">\
+				<div>\
+					<h3>' + staff[i].name + '</h3>\
+					<div class="details">\
+						<img src="' + staff[i].clear + '" alt="' + staff[i].name + '">\
+							<p>' + staff[i].bio + '</p>\
+							</div>\
+						</div>\
+					</div>');
+		    }
+		}
 
-        switch ($(this).attr('id')) {
-            case 'aisha':
-                profile = 'is a crafty designer who perpetually strives for pixel perfection. She believes that the first idea is never the best. Often she isn’t content until she has made upwards of 40 revisions. With the help of some Pixeltwist coworkers, she is peeled away from the computer screen long enough to enjoy a green tea frappuccino. That moment is fleeting though, as she quickly finds her way back to her computer to start on revision 41.';
-                break;
-            case 'keenan':
-                profile = 'enjoys life through the view of a camera lens. Everywhere he goes he sees shot angles, backdrops and set pieces. It’s this half-man, half-camera cyborg that leads the video department at Pixeltwist. When he isn’t in the studio producing amazing video for clients, Keenan enjoys expanding his already expanded movie knowledge.';
-                break;
-            case 'kevin':
-                profile = 'refers to himself as the web wizard at Pixeltwist. While it may seem a touch immodest to refer to himself as a wizard, his talent for flawless code cannot be denied. Some say his ability comes from being a direct descendant of an ancient coding god. The reality is that nobody has seen Kevin leave the studio in a long while. It is believed that he naps in the ceiling at night and emerges in the wee hours of the morning to feed his coding compulsions.';
-                break;
-            case 'lulu':
-                profile = 'is project-lead at Pixeltwist. She’s plugged in to everything going on through each day. When a client comes looking for our expertise, she’s spearheading the meeting to extract the perfect idea. Her attention to detail extends to the kitchen, where she often brings some of her masterfully-prepared home-cooked food to the office for everyone to enjoy.';
-                break;
-            case 'michael':
-                profile = 'is the User Experience Designer in charge of keeping the peace between design and usability. His passion is in walking the fine line between a beautiful design and a smart design. His word is the law with keeping UX beautiful, but above all else, functional. So if any designer hopes to get their work approved, they have to get past Michael first.';
-                break;
-            case 'robb':
-                profile = 'is the lead audio guy at Pixeltwist. Much like a dog can pick up sounds that humans can’t, he can pick up minute imperfections in sound that the rest of us mere mortals cannot. During his job interview we had to take his word for it and hire him. Robb’s passion was forged through years of being a musician and music lover.';
-                break;
-            case 'vanessa':
-                profile = 'is the savvy web designer at Pixeltwist. She’s the web developer embodiment of a swiss army knife. She can knock out fresh new ideas in photoshop, mock them up to work on the web, then present them to the client all in a days work. But aside from school and years of professional experience, where does she get such ability? The word is her sketchbook was imbued by ancient gypsies granting her mystical web development skill.';
-            case 'zach':
-                profile = 'is the content manager at Pixeltwist. Wrangling words like a skillful literary cowboy is how he spends his day. But to put it in plain english, he is the lead copywriter and walking, talking dictionary at the studio. The creative part of Zach’s brain grows three sizes when he’s twisting words for clients at Pixeltwist.';
-                break;
-            default:
-                break;
-        }
-
-        var overlay = $('<div id="overlay"><div class="profile"><img src="images/team/clear/' + $(this).attr('id') + '.jpg" alt="' + $(this).attr('value') + '"><div><h3>' + $(this).attr('value') + '</h3><p>' + profile + '</p></div></div></div>');
-
-        overlay.appendTo(document.body).show('slow');
+		$('body').append(overlay);
 
         if ($(this).length != 0) {
             $(overlay).on('click', function() {
